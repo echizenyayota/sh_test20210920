9bd84ef47b32c718c10afd14a35fdc977970760c
echizenyayota <virtualmisso30@gmail.com>
2021-09-20 16:19:15 +0900

    ResourcePickerの追加（TitleBarの追加まで）

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import { Heading, Page, TextStyle, Layout, Card, EmptyState } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
+    <TitleBar 
+      primaryAction={{
+        content: 'Select Product',
+        onAction: () => this.setState({ open: true }),
+      }}
+    />
+    <ResourcePicker
+
+    />
     <Layout>
       <EmptyState
         heading="Discout your products"
