7c935d2af641794d0ff5954704b4ffeda36e8d22
echizenyayota <virtualmisso30@gmail.com>
2021-09-20 14:14:58 +0900

    Hello Worldの表示

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+// import { Heading, Page } from "@shopify/polaris";
+
+// const Index = () => (
+//   <Page>
+//     <Heading>Shopify app with Node and React 🎉</Heading>
+//   </Page>
+// );
+
+// export default Index;
+
 import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Heading>Hello World!</Heading>
   </Page>
 );
 
 export default Index;
+
+
+
